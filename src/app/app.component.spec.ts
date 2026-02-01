52dff778797993a72c96b444b31160bc49c677c7
Julio Algouver <julioalgouver@gmail.com>
2026-02-01 13:49:17 -0300

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the '2-desafio-plataforma-usuario' title`, () => {
+  it(`should have the '2-desafio-plataforma-usuario-resolucao' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('2-desafio-plataforma-usuario');
+    expect(app.title).toEqual('2-desafio-plataforma-usuario-resolucao');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 2-desafio-plataforma-usuario');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 2-desafio-plataforma-usuario-resolucao');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = '2-desafio-plataforma-usuario';
+  title = '2-desafio-plataforma-usuario-resolucao';
 }
